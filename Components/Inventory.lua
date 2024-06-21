9d9aee5fcfe2f05604a035727107db848c912328
furtrade <tov77@protonmail.com>
2024-06-22 00:14:30 +1000

    Refactor Inventory.lua

--- Components/Inventory.lua
+++ Components/Inventory.lua
@@ -1,6 +1,6 @@
 local addonName, addon = ...
 
--- addon.itemCache = addon.itemCache or {}
+addon.itemCache = addon.itemCache or {}
 
 local function getItemLink(bagOrSlotIndex, slotIndex)
     return slotIndex and C_Container.GetContainerItemLink(bagOrSlotIndex, slotIndex)
@@ -40,7 +40,9 @@ local function addItemToCache(bagOrSlotIndex, slotIndex)
     local itemLink = getItemLink(bagOrSlotIndex, slotIndex)
     if not itemLink then return end
 
-    for _, cachedItem in ipairs(addon.itemCache) do
+    local cache = addon.itemCache
+    for i = 1, #cache do
+        local cachedItem = cache[i]
         if cachedItem.link == itemLink then
             cachedItem.count = GetItemCount(cachedItem.id)
             cachedItem.reviewed = true
@@ -50,29 +52,36 @@ local function addItemToCache(bagOrSlotIndex, slotIndex)
 
     local itemInfo = itemizer(bagOrSlotIndex, slotIndex)
     if itemInfo then
-        table.insert(addon.itemCache, itemInfo)
+        table.insert(cache, itemInfo)
     end
 end
 
 function addon:UpdateItemCache()
-    for _, item in ipairs(addon.itemCache) do
-        item.reviewed = false
+    local cache = addon.itemCache
+
+    -- Mark all items in the cache as not reviewed
+    for i = 1, #cache do
+        cache[i].reviewed = false
     end
 
+    -- Update equipped items
     for bagOrSlotIndex = 1, 19 do
         addItemToCache(bagOrSlotIndex)
     end
 
+    -- Update bag items
+    local GetContainerNumSlots = C_Container.GetContainerNumSlots
     for bagOrSlotIndex = 0, Constants.InventoryConstants.NumBagSlots do
-        local numSlots = C_Container.GetContainerNumSlots(bagOrSlotIndex)
+        local numSlots = GetContainerNumSlots(bagOrSlotIndex)
         for slotIndex = 1, numSlots do
             addItemToCache(bagOrSlotIndex, slotIndex)
         end
     end
 
-    for i = #addon.itemCache, 1, -1 do
-        if not addon.itemCache[i].reviewed then
-            table.remove(addon.itemCache, i)
+    -- Remove items from the cache that were not reviewed
+    for i = #cache, 1, -1 do
+        if not cache[i].reviewed then
+            table.remove(cache, i)
         end
     end
 end
