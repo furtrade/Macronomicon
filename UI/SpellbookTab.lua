db02c0b8687aa9f555861165e50bd309a8ecd30a
furtrade <tov77@protonmail.com>
2024-07-03 20:46:08 +1000

    feat: add glowing clicked state to tab buttons to mimic default ux

--- UI/SpellbookTab.lua
+++ UI/SpellbookTab.lua
@@ -14,12 +14,21 @@ function CreateSpellbookTab()
     -- Determine the number of existing tabs
     local numTabs = GetNumSpellTabs()
 
-    local tab = CreateFrame("Button", "MacrobialSpellbookTab", SpellBookFrame, "SpellBookSkillLineTabTemplate")
+    local tab = CreateFrame("CheckButton", "MacrobialSpellbookTab", SpellBookFrame, "SpellBookSkillLineTabTemplate")
     tab:SetPoint("TOPLEFT", _G["SpellBookSkillLineTab" .. numTabs], "BOTTOMLEFT", 0, -17)
     tab:SetNormalTexture("Interface\\Icons\\INV_Misc_QuestionMark")
     tab.tooltip = "Macrobial"
 
     tab:SetScript("OnClick", function(self)
+        -- Hide default spellbook content
+        for i = 1, numTabs do
+            local defaultTab = _G["SpellBookSkillLineTab" .. i]
+            if defaultTab then
+                defaultTab:SetChecked(false)
+            end
+        end
+        self:SetChecked(true)
+
         -- Show custom spellbook frame
         if not MacrobialSpellbookFrame then
             addon:CreateMacrobialSpellbookFrame()
@@ -39,14 +48,23 @@ function CreateSpellbookTab()
 
     tab:Show()
 
-    -- Hook tab clicks to hide the Macrobial frame when other tabs are clicked
+    -- Hook tab clicks to hide the Macrobial frame and manage glow borders
     for i = 1, numTabs do
         local defaultTab = _G["SpellBookSkillLineTab" .. i]
         if defaultTab then
             defaultTab:HookScript("OnClick", function()
+                MacrobialSpellbookTab:SetChecked(false)
                 if MacrobialSpellbookFrame then
                     MacrobialSpellbookFrame:Hide()
                 end
+                -- Set checked state to the clicked tab
+                for j = 1, numTabs do
+                    local otherTab = _G["SpellBookSkillLineTab" .. j]
+                    if otherTab then
+                        otherTab:SetChecked(false)
+                    end
+                end
+                defaultTab:SetChecked(true)
                 -- Clear any tooltips
                 GameTooltip:Hide()
             end)
